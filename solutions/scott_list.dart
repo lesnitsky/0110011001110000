ee7b59672f5f9a93d5c24ae7fba8154d7724e8fa
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2020-08-05 13:27:20 +0300

    implement list length using church numerals
    #13

--- solutions/scott_list.dart
+++ solutions/scott_list.dart
@@ -1,8 +1,11 @@
+import 'church_encoding.dart';
+
 var Nil = (n) => (c) => n;
 var cons = (h) => (t) => (n) => (c) => c(h)(t);
 var uncons = (t) => (n) => (c) => t(n)(c);
 
 var _length = (l) => uncons(l)(0)((h) => (t) => 1 + _length(t));
+var _lengthC = (l) => uncons(l)(zero)((_) => (t) => succ(_lengthC(t)));
 
 extension List on Function {
   prepend(dynamic value) {
@@ -10,15 +13,19 @@ extension List on Function {
   }
 
   int get length => _length(this);
+  int get lengthC => (_lengthC(this) as Function).toNumber();
 }
 
 void main() {
   var list = Nil;
   assert(list.length == 0);
+  assert(list.lengthC == list.length);
 
   list = list.prepend(42);
   assert(list.length == 1);
+  assert(list.lengthC == list.length);
 
   list = list.prepend(15);
   assert(list.length == 2);
+  assert(list.lengthC == list.length);
 }
