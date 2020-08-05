8e43b8ccca21b5e77a6dc480fa8e6c4f9a52bb9a
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2020-08-05 13:30:18 +0300

    implement head for list
    #16

--- solutions/scott_list.dart
+++ solutions/scott_list.dart
@@ -6,26 +6,31 @@ var uncons = (t) => (n) => (c) => t(n)(c);
 
 var _length = (l) => uncons(l)(0)((h) => (t) => 1 + _length(t));
 var _lengthC = (l) => uncons(l)(zero)((_) => (t) => succ(_lengthC(t)));
+var _head = (l) => uncons(l)(null)((h) => (t) => h);
 
-extension List on Function {
+extension List<T> on Function {
   prepend(dynamic value) {
     return cons(value)(this);
   }
 
   int get length => _length(this);
   int get lengthC => (_lengthC(this) as Function).toNumber();
+  T get head => _head(this);
 }
 
 void main() {
   var list = Nil;
   assert(list.length == 0);
   assert(list.lengthC == list.length);
+  assert(list.head == null);
 
   list = list.prepend(42);
   assert(list.length == 1);
   assert(list.lengthC == list.length);
+  assert(list.head == 42);
 
   list = list.prepend(15);
   assert(list.length == 2);
   assert(list.lengthC == list.length);
+  assert(list.head == 15);
 }
