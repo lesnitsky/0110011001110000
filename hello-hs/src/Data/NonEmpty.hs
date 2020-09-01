085586435a97775a71f7d3bbe7818ff05c826b4c
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2020-09-01 15:42:20 +0300

    implement tail, init and last for NonEmpty
    #25

--- hello-hs/src/Data/NonEmpty.hs
+++ hello-hs/src/Data/NonEmpty.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE BlockArguments #-}
 module Data.NonEmpty where
 
-import           Data.List (List (..), (<|))
-import qualified Data.List as L
-import           Prelude   (Eq, Int, Show, (+), (==))
+import           Data.List  (List (..), (<|))
+import qualified Data.List  as L
+import           Data.Maybe (Maybe (Just))
+import           Data.Maybe (Maybe (Nothing))
+import           Prelude    (Eq, Int, Show, (+), (==))
 
 data NonEmpty a = a :| (List a)
   deriving (Show)
@@ -18,6 +21,34 @@ length (_ :| t) = 1 + L.length t
 
 head :: NonEmpty a -> a
 head (h :| _) = h
+
+-- >>> Data.NonEmpty.head l
+-- 1
+
+tail :: NonEmpty a -> List a
+tail (_ :| t) = t
+
+-- >>> Data.NonEmpty.tail l
+-- [2, 3]
+
+init :: NonEmpty a -> List a
+init (_ :| Nil) = Nil
+init (h :| t) = Cons h case (L.init t) of
+  Just t' -> t'
+  Nothing -> Nil
+
+-- >>> Data.NonEmpty.init l
+-- [1, 2]
+
+-- >>> Data.NonEmpty.init (1 :| Nil)
+-- []
+
+last :: NonEmpty a -> a
+last (h :| Nil) = h
+last (_ :| t)   = let Just t' = L.last t in t'
+
+-- >>> Data.NonEmpty.last (2 :| (3 <| 4 <| Nil))
+-- 4
 
 group :: Eq a => List a -> List (NonEmpty a)
 group Nil        = Nil
