a3ec8559f1a6af200bec7876f86ea258d6d4d9b9
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2020-09-02 16:37:03 +0300

    RLE the whole universe

--- hello-hs/src/Encoding/RLE.hs
+++ hello-hs/src/Encoding/RLE.hs
@@ -5,17 +5,17 @@ import           Data.List        (List (Nil), map, (<|))
 import           Data.List        (replicate)
 import           Data.List        (concatMap)
 import           Data.NonEmpty    (group, head, length)
-import           Prelude          (Char, Int, fst, snd, uncurry)
+import           Prelude          (Char, Eq, Int, fst, snd, uncurry)
 
-type RLE = List (Int, Char)
+type RLE a = List (Int, a)
 
-rle :: List Char -> RLE
+rle :: Eq a => List a -> RLE a
 rle = map (\list -> (length list, head list)) <<< group
 
 -- >>> rle "aaaabbccc"
 -- [(4,'a'), (2,'b'), (3,'c')]
 
-unrle :: RLE -> List Char
+unrle :: RLE a -> List a
 unrle = concatMap (uncurry replicate)
 
 -- >>> unrle ((2,'a') <| (3, 'b') <| Nil)
