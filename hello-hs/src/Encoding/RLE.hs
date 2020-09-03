c2efc5d80b27e95537cd71b64347b8f1c0eae1d4
Alexander Gerasimov <leo.daquirm@gmail.com>
2020-09-03 15:57:43 +0200

    Remove unnecessary imports; fix rle test (#27)

--- hello-hs/src/Encoding/RLE.hs
+++ hello-hs/src/Encoding/RLE.hs
@@ -1,18 +1,18 @@
 module Encoding.RLE where
 
 import           Control.Category ((<<<))
-import           Data.List        (List (Nil), map, (<|))
+import           Data.List        (List(Nil), map, (<|))
 import           Data.List        (replicate)
 import           Data.List        (concatMap)
 import           Data.NonEmpty    (group, head, length)
-import           Prelude          (Char, Eq, Int, fst, snd, uncurry)
+import           Prelude          (Eq, Int, uncurry)
 
 type RLE a = List (Int, a)
 
 rle :: Eq a => List a -> RLE a
 rle = map (\list -> (length list, head list)) <<< group
 
--- >>> rle "aaaabbccc"
+-- >>> rle ("aaaabbccc" :: List Char)
 -- [(4,'a'), (2,'b'), (3,'c')]
 
 unrle :: RLE a -> List a
