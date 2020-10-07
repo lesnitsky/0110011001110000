9f2a570b7c558746a492a1d22f8623b8c302cbf2
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2020-10-07 19:58:42 +0300

    implement list literals

--- hello-hs/src/Data/List.hs
+++ hello-hs/src/Data/List.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE OverloadedLists #-}
 
 module Data.List where
 
@@ -8,16 +10,17 @@ import           Data.String (IsString (fromString))
 import           Prelude     (Bool (..), Char, Eq, Int, Show (show), String,
                               fst, otherwise, snd, (*), (+), (++), (-), (==),
                               (||))
+import GHC.Exts (Item, IsList (fromList, toList))
 
 data List a
   = Nil
   | Cons a (List a)
 
 l3 :: List Int
-l3 = Cons 2 (Cons 1 (Cons 0 Nil))
+l3 = [2, 1, 0]
 
 length :: List a -> Int
-length Nil        = 0
+length []        = 0
 length (Cons _ l) = 1 + length l
 
 -- >>> Data.List.length Nil
@@ -27,7 +30,7 @@ length (Cons _ l) = 1 + length l
 -- 3
 
 isEmpty :: List a -> Bool
-isEmpty Nil = True
+isEmpty [] = True
 isEmpty _   = False
 
 -- >>> isEmpty l3
@@ -37,14 +40,14 @@ isEmpty _   = False
 -- True
 
 concat :: List a -> List a -> List a
-concat Nil b        = b
+concat [] b        = b
 concat (Cons h t) b = h <| concat t b
 
 -- >>> Data.List.concat (1 <| 2 <| Nil) (3 <| 4 <| Nil)
 -- [1, 2, 3, 4]
 
 concatMap :: (a -> List b) -> List a -> List b
-concatMap _ Nil        = Nil
+concatMap _ []        = []
 concatMap f (Cons h t) = concat (f h) (concatMap f t)
 
 -- >>> Data.List.concatMap (Data.List.replicate 5) (1 <| 2 <| Nil)
@@ -52,17 +55,17 @@ concatMap f (Cons h t) = concat (f h) (concatMap f t)
 
 instance IsString (List Char) where
   fromString :: String -> List Char
-  fromString ""      = Nil
+  fromString ""      = []
   fromString (h : t) = Cons h (fromString t)
 
 joinString :: Show a => String -> List a -> String
-joinString _ Nil          = ""
-joinString _ (Cons h Nil) = show h
+joinString _ []          = ""
+joinString _ [h] = show h
 joinString d (Cons h t)   = show h ++ d ++ joinString d t
 
 join :: Show a => List Char -> List a -> List Char
-join _ Nil          = Nil
-join _ (Cons h Nil) = fromString (show h)
+join _ []          = []
+join _ [h] = fromString (show h)
 join d (Cons h t)   = concat (concat (fromString (show h)) d) (join d t)
 
 -- >>> join ", " (1 <| 2 <| Nil)
@@ -79,7 +82,7 @@ instance Show a => Show (List a) where
 -- "[1]"
 
 contains :: Eq a => a -> List a -> Bool
-contains _ Nil        = False
+contains _ []        = False
 contains a (Cons h t) = h == a || contains a t
 
 -- >>> contains 1 (Cons 1 Nil)
@@ -96,46 +99,46 @@ infixr 5 <|
 -- True
 
 head :: List a -> Maybe a
-head Nil        = Nothing
+head []        = Nothing
 head (Cons h _) = Just h
 
 -- >>> Data.List.head (1 <| 2 <| 3 <| Nil)
 -- Just 1
 
 group :: Eq a => List a -> List (List a)
-group Nil        = Nil
+group []        = []
 group (Cons h t) = let (s, t') = span (\x -> x == h) t in (h <| s) <| group t'
 
 -- >>> Data.List.group (1 <| 1 <| 2 <| 1 <| 3 <| 3 <| 2 <| Nil)
 -- [[1, 1], [2], [1], [3, 3], [2]]
 
 map :: (a -> b) -> List a -> List b
-map _ Nil        = Nil
+map _ []        = []
 map f (Cons h t) = f h <| map f t
 
 span :: (a -> Bool) -> List a -> (List a, List a)
-span _ Nil = (Nil, Nil)
+span _ [] = ([], [])
 span f l@(Cons h t) | f h = let (l', r) = span f t in (h <| l', r)
-                    | otherwise = (Nil, l)
+                    | otherwise = ([], l)
 -- >>> Data.List.span (< 5) (1 <| 2 <| 7 <| 9 <| 4 <| 1 <| Nil)
 -- ([1, 2],[7, 9, 4, 1])
 
 sum :: List Int -> Int
-sum Nil        = 0
+sum []        = 0
 sum (Cons h t) = h + sum t
 
 -- >>> Data.List.sum (1 <| 2 <| 3 <| Nil)
 -- 6
 
 product :: List Int -> Int
-product Nil        = 1
+product []        = 1
 product (Cons h t) = h * (product t)
 
 -- >>> Data.List.product (1 <| 2 <| 3 <| 4 <| Nil)
 -- 24
 
 tail :: List a -> Maybe (List a)
-tail Nil        = Nothing
+tail []        = Nothing
 tail (Cons _ t) = Just t
 
 -- >>> Data.List.tail (1 <| 2 <| 3 <| 4 <| Nil)
@@ -145,8 +148,8 @@ tail (Cons _ t) = Just t
 -- Nothing
 
 last :: List a -> Maybe a
-last Nil          = Nothing
-last (Cons h Nil) = Just h
+last []          = Nothing
+last [h] = Just h
 last (Cons _ t)   = last t
 
 -- >>> Data.List.last (1 <| 2 <| 3 <| 4 <| Nil)
@@ -156,11 +159,11 @@ last (Cons _ t)   = last t
 -- Just 1
 
 init :: List a -> Maybe (List a)
-init Nil          = Nothing
-init (Cons _ Nil) = Just Nil
+init []          = Nothing
+init (Cons _ []) = Just []
 init (Cons h t)   = case init t of
   Just l  -> Just (Cons h l)
-  Nothing -> Just (Cons h Nil)
+  Nothing -> Just [h]
 
 -- >>> Data.List.init (1 <| 2 <| 3 <| Nil)
 -- Just [1, 2]
@@ -169,17 +172,28 @@ init (Cons h t)   = case init t of
 -- Just []
 
 replicate :: Int -> a -> List a
-replicate 0 _ = Nil
+replicate 0 _ = []
 replicate n v = v <| (replicate (n - 1) v)
 
 -- >>> Data.List.replicate 4 "a"
 -- ["a", "a", "a", "a"]
 
 reverse :: List a -> List a
-reverse Nil        = Nil
+reverse []        = []
 reverse (Cons h t) = case init t of
-  Nothing -> h <| Nil
-  Just _  -> concat (reverse t) (h <| Nil)
+  Nothing -> h <| []
+  Just _  -> concat (reverse t) (h <| [])
 
 -- >>> Data.List.reverse (1 <| 2 <| 3 <| 4 <| Nil)
 -- [4, 3, 2, 1]
+
+instance IsList (List a) where
+  type Item (List a) = a
+
+  fromList :: [Item (List a)] -> List a
+  fromList [] = Nil
+  fromList (h:t) = Cons h (fromList t)
+
+  toList :: List a -> [Item (List a)]
+  toList Nil = []
+  toList (Cons h t) = h : toList t
